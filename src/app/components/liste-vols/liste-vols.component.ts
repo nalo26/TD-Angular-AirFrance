3feb1c8d98e870c75523c4e21d2a0757047c4f34
Émilie Vey <nath.v26@gmail.com>
2022-05-09 11:21:19 +0200

    Listed flights
    Co-authored-by: nathan.peyronnet@gmail.com <nathan.peyronnet@gmail.com>

--- src/app/components/liste-vols/liste-vols.component.ts
+++ src/app/components/liste-vols/liste-vols.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Vol } from 'src/app/models/vol.model';
 
 @Component({
   selector: 'app-liste-vols',
   templateUrl: './liste-vols.component.html',
-  styleUrls: ['./liste-vols.component.scss']
+  styleUrls: ['./liste-vols.component.scss'],
 })
 export class ListeVolsComponent {
+  @Input() vols!: Vol[] | undefined;
+  @Output() vol_emitter : EventEmitter<Vol> = new EventEmitter()
 
+
+
+  click(vol: Vol): void {
+    this.vol_emitter.emit(vol);
+  }
 }
--- src/app/components/vol/vol.component.ts
+++ src/app/components/vol/vol.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Vol } from 'src/app/models/vol.model';
 
 @Component({
   selector: 'app-vol',
   templateUrl: './vol.component.html',
-  styleUrls: ['./vol.component.scss']
+  styleUrls: ['./vol.component.scss'],
 })
 export class VolComponent {
-
+  @Input() vol!: Vol;
 }
